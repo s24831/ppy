0ded62343bed1c67fc6e88589b3a17ee4a543336
sikorka-png <sikora.mateo@gmail.com>
2023-03-10 19:34:44 +0100

    requests method

--- 01/main.py
+++ 01/main.py
@@ -1,6 +1,19 @@
 import webbrowser
+import requests
 
-if __name__ == '__main__':
-    print("Hello world")
-    url = input("Podaj strone internetowa")
-    webbrowser.open(url)
+for i in range(3):
+    pageUrl = input("Podaj adres strony: ")
+    date = input("Podaj date: ")
+
+    url = "http://archive.org/wayback/available?url=" + pageUrl + "&timestamp=" + str(date)
+
+    page = requests.get(url).json()["archived_snapshots"]["closest"]["url"]
+    webbrowser.open(page)
+    print()
+
+
+# W Pythonie kod zrodłowy jest przetwarzany przez interpreter, ktory przetwarza kod linia po linii w czasie rzeczywistym,a następnie wykonuje go bezposrednio
+# Interpreter jest programem, ktory tlumaczy kod zrodlowy na kod maszynowy
+
+# W Java kod źródłowy jest kompilowany do kodu bajtowego, który jest następnie wykonywany przez maszynę wirtualną Javy
+# JVM jest programem, ktory dziala multiplatformowo i zapewnia warstwe miedzy kodem bajtowym a komputerem
