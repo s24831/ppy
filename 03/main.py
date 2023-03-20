a931b47b98f23c7ef4f038dbbcf4e2e01db4ff01
Mateusz Sikora <s24831@pjwstk.edu.pl>
2023-03-20 15:16:13 +0100

    finished 0.9

--- 03/main.py
+++ 03/main.py
@@ -1,8 +1,8 @@
-from collections import Counter
 import random
+import getpass
 
 if __name__ == '__main__':
-    #zadanie 1
+    # zadanie 1
     data = input("podaj liczby: ").split(",")
     max_number = data[0]
     min_number = data[0]
@@ -14,7 +14,7 @@ if __name__ == '__main__':
     print(f"max number: {max_number}")
     print(f"min number: {min_number}")
 
-    #zadanie 2
+    # zadanie 2
     cities = [
         "miasto 1",
         "miasto 2",
@@ -35,56 +35,65 @@ if __name__ == '__main__':
         not_to_use.append(rand)
         print(f"Miaso {i}: {cities[rand]}")
 
-    #zadanie 3
+    # zadanie 3
     rounds = int(input("rounds: "))
     mode = input("single/multi: ")
-    score = []
+    score = {
+        0: 0,
+        1: 0,
+        2: 0
+    }
     if mode == "multi":
         player1_name = input("player 1 name: ")
         player2_name = input("player 2 name: ")
         print("start")
         for i in range(rounds):
-            rps1 = input(f"{player1_name}: ")
-            rps2 = input(f"{player2_name}: ")
+            print(f"{player1_name} ", end="")
+            rps1 = getpass.getpass("choice: ")
+            print(f"{player2_name} ", end="")
+            rps2 = getpass.getpass("choice: ")
             if rps1 == rps2:
                 print("no one won")
-                score.append(0)
+                score[0] += 1
             elif rps1 == "rock" and rps2 == "scissors":
                 print(f"{player1_name} won")
-                score.append(1)
+                score[1] += 1
             elif rps1 == "paper" and rps2 == "rock":
                 print(f"{player1_name} won")
-                score.append(1)
+                score[1] += 1
             elif rps1 == "scissors" and rps2 == "paper":
                 print(f"{player1_name} won")
-                score.append(1)
+                score[1] += 1
             else:
                 print(f"{player2_name} won")
-                score.append(2)
+                score[2] += 1
     else:
         choices = ["rock", "paper", "scissors"]
-        player1_name = choices[random.randrange(2)]
+        player1_name = input("computer name: ")
         player2_name = input("player 2 name: ")
         print("start")
         for i in range(rounds):
-            rps1 = input("player 1: ")
+            rps1 = choices[random.randrange(2)]
             rps2 = input("player 2: ")
             if rps1 == rps2:
                 print("no one won")
-                score.append(0)
+                score[0] += 1
             elif rps1 == "rock" and rps2 == "scissors":
                 print(f"{player1_name} won")
-                score.append(1)
+                score[1] += 1
             elif rps1 == "paper" and rps2 == "rock":
                 print(f"{player1_name} won")
-                score.append(1)
+                score[1] += 1
             elif rps1 == "scissors" and rps2 == "paper":
                 print(f"{player1_name} won")
-                score.append(1)
+                score[1] += 1
             else:
                 print(f"{player2_name} won")
-                score.append(2)
-    who_won = Counter(score).most_common()
+                score[2] += 1
+
+    who_won = max(score, key=lambda l: score[l]) 
+    print(score)
+    print(who_won)
     if who_won == 0:
         print("no one won")
     elif who_won == 1:
