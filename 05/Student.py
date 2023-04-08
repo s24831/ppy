99a7a2729a9cf71870709f4e4cb7895a93b0f7f1
s24831 <sikora.mateo@gmail.com>
2023-04-08 14:09:59 +0200

    100%

--- 05/Student.py
+++ 05/Student.py
@@ -30,19 +30,24 @@ class Student:
                     student.ocena_koncowa = 4.5
                 else:
                     student.ocena_koncowa = 5
+            student.staus = 'GRADED'
+        cls.save_to_file()
 
     @classmethod
     def add_new_student(cls, email: str, imie: str, nazwisko: str, punkty: float, ocena_koncowa=None, status=None):
         for s in cls.students:
             if s.email == email:
                 return "email zajety"
-        return Student(email, imie, nazwisko, punkty, ocena_koncowa, status)
+        s = Student(email, imie, nazwisko, punkty, ocena_koncowa, status)
+        cls.save_to_file()
+        return s
 
     @classmethod
     def delete_student(cls, email: str):
         for i, s in enumerate(cls.students):
             if s.email == email:
                 cls.students.pop(i)
+        cls.save_to_file()
 
     @classmethod
     def send_email(cls, subject, body, sender, password):
@@ -59,6 +64,13 @@ class Student:
         smtp_server.login(sender, password)
         smtp_server.sendmail(sender, recipients, msg.as_string())
         smtp_server.quit()
+        cls.save_to_file()
+
+    @classmethod
+    def save_to_file(cls):
+        with open(f".\students.txt", 'w') as file:
+            for s in cls.students:
+                file.write(f"{s}\n")
 
     def __repr__(self):
-        return f"{self.email}, {self.imie}, {self.nazwsiko}, {self.punkty}, {self.ocena_koncowa}, {self.staus}"
+        return f"{self.email},{self.imie},{self.nazwsiko},{self.punkty},{self.ocena_koncowa},{self.staus}"
